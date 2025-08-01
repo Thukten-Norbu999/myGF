04a43150e2a2c404a4c83d969ca7a64efa52b980
Thukten Norbu <thuktennorbu33@gmail.com>
2025-08-01 14:07:33 +0800

    letter

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,36 @@
+// Initialize tries if not set
+if (!localStorage.getItem("tries")) {
+  localStorage.setItem("tries", "3");
+}
 
-CacheStorage.bind()
+// Get tries as a number
+let tries = parseInt(localStorage.getItem("tries"), 10);
 
+function CheckCorrect(e) {
+  e.preventDefault();
 
+  // Get input value
+  const inputVal = document.getElementById("need").value;
 
+  console.log("Tries left:", tries);
 
-function CheckCorrect(e){
-    e.preventDefault()
-    if (document.getElementById("need").value == "25/04/2025"){
-        window.location.href = "/letter.html"
-        console.log("preesed true")
+  if (inputVal === "25/04/2025") {
+    // Correct answer - redirect
+    window.location.href = "./letter.html";
+  } else {
+    // Wrong answer - decrement tries
+    tries -= 1;
+
+    if (tries > 0) {
+      alert(`Try again! You have ${tries} tries left.`);
+    } else {
+      alert("Code is 25/04/2025");
+      // Optionally disable the button after tries run out
+      
     }
-    console.log('f',document.getElementById("need").value)
+    localStorage.setItem("tries", tries.toString());
+  }
 }
 
 document.getElementById("submit_1").addEventListener("click", CheckCorrect);
+
